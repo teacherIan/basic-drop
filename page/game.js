516ef7f3dd54cd519bf4c0315284e6119a91dbcd
teacherIan <ian_malloy@ssis.asia>
2022-12-30 22:58:26 +0800

    Add: Loading Screen

--- page/game.js
+++ page/game.js
@@ -5,65 +5,74 @@ import music from '../assets/alexander-nakarada-superepic.mp3';
 import { Experience } from './Experience';
 const container = document.querySelector('.game-container');
 const startButton = document.querySelector('.start-button');
+const loadingText = document.querySelector('.loading-text');
 
 let total = 0;
 fetch('https://house-points.onrender.com/get')
   .then((response) => response.json())
-  .then((data) => {
-    data.forEach((house) => {
-      total = total + house.points;
+  .then(
+    (data) => {
+      data.forEach((house) => {
+        loadingText.style.display = 'none';
+        total = total + house.points;
 
-      if (house.house == 'Ruby') {
-        settings.rubyMax = house.points;
+        if (house.house == 'Ruby') {
+          settings.rubyMax = house.points;
+        }
+        if (house.house == 'Sapphire') {
+          settings.sapphireMax = house.points;
+        }
+        if (house.house == 'Amber') {
+          settings.amberMax = house.points;
+        }
+        if (house.house == 'Pearl') {
+          settings.pearlMax = house.points;
+        }
+      });
+      if (total < 1000) {
+        settings.gameSpeed = 200;
+        settings.ballSize = 12;
+        settings.scaleFactor = 0.01;
+        settings.largeGame = false;
       }
-      if (house.house == 'Sapphire') {
-        settings.sapphireMax = house.points;
+      if (total > 1000) {
+        settings.gameSpeed = 200;
+        settings.ballSize = 6.8;
+        settings.scaleFactor = 0.01;
       }
-      if (house.house == 'Amber') {
-        settings.amberMax = house.points;
+      if (total > 2000) {
+        settings.gameSpeed = 150;
+        settings.ballSize = 6.4;
+        settings.scaleFactor = 0.01;
       }
-      if (house.house == 'Pearl') {
-        settings.pearlMax = house.points;
+      if (total > 3000) {
+        settings.gameSpeed = 120;
+        settings.ballSize = 5.5;
+        settings.scaleFactor = 0.01;
       }
-    });
-    if (total < 1000) {
-      settings.gameSpeed = 200;
-      settings.ballSize = 12;
-      settings.scaleFactor = 0.01;
-      settings.largeGame = false;
+      if (total > 4000) {
+        settings.gameSpeed = 110;
+        settings.ballSize = 5;
+        settings.scaleFactor = 0.02;
+      }
+      if (total > 5000) {
+        settings.gameSpeed = 100;
+        settings.ballSize = 4.7;
+        settings.scaleFactor = 0.05;
+      }
+      if (total > 6000) {
+        settings.gameSpeed = 70;
+        settings.ballSize = 4.3;
+        settings.scaleFactor = 0.05;
+      }
+      startButton.style.display = 'block';
+    },
+    (err) => {
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
     }
-    if (total > 1000) {
-      settings.gameSpeed = 200;
-      settings.ballSize = 6.8;
-      settings.scaleFactor = 0.01;
-    }
-    if (total > 2000) {
-      settings.gameSpeed = 150;
-      settings.ballSize = 6.4;
-      settings.scaleFactor = 0.01;
-    }
-    if (total > 3000) {
-      settings.gameSpeed = 120;
-      settings.ballSize = 5.5;
-      settings.scaleFactor = 0.01;
-    }
-    if (total > 4000) {
-      settings.gameSpeed = 110;
-      settings.ballSize = 5;
-      settings.scaleFactor = 0.02;
-    }
-    if (total > 5000) {
-      settings.gameSpeed = 100;
-      settings.ballSize = 4.7;
-      settings.scaleFactor = 0.05;
-    }
-    if (total > 6000) {
-      settings.gameSpeed = 70;
-      settings.ballSize = 4.3;
-      settings.scaleFactor = 0.05;
-    }
-    startButton.style.display = 'block';
-  });
+  );
 
 startButton.addEventListener('click', () => {
   if (container.requestFullscreen) {
